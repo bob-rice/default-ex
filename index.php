6a7fc3612d43795d1092e40295d42f805e94d4c6
Bobrice <cbo142857@gmail.com>
2018-03-12 15:43:13 +0900

    1st setting values
    .

---
+++ index.php
@@ -0,0 +1,10 @@
+<!-- php header/footer 불러오기 구문 -->
+<? php
+	include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
+?>
+<div id="container">
+	container
+</div>
+<? php
+	include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";
+?>
