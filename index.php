1c6ac74e070097e2df3774026b83ab928c8079dc
Bobrice <cbo142857@gmail.com>
2018-03-12 16:05:22 +0900

    test01-1
    .

--- index.php
+++ index.php
@@ -1,10 +1,10 @@
 <!-- php header/footer 불러오기 구문 -->
 <? php
-	include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
+	include $_SERVER["DOCUMENT_ROOT"]."./inc/header.php";
 ?>
 <div id="container">
 	<div></div>
 </div>
 <? php
-	include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";
+	include $_SERVER["DOCUMENT_ROOT"]."./inc/footer.php";
 ?>
