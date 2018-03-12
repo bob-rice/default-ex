976d9d33f0ca56713ad4dfddaf287a8d65e87fb6
Bobrice <cbo142857@gmail.com>
2018-03-12 16:18:25 +0900

    .
    .

--- index.php
+++ index.php
@@ -1,9 +1,9 @@
 <?php
-	include $_SERVER["DOCUMENT_ROOT"]."./inc/header.php";
+	include $_SERVER["DOCUMENT_ROOT"]."/inc/header.php";
 ?>
 <div id="container">
 	<div></div>
 </div>
 <?php
-	include $_SERVER["DOCUMENT_ROOT"]."./inc/footer.php";
+	include $_SERVER["DOCUMENT_ROOT"]."/inc/footer.php";
 ?>
